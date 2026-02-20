5b731688653965fa4f0a8aa5670309c16bd78e95
Sneha Walunj <snehawalunj01@gmail.com>
2026-02-20 10:37:54 +0000

    updated RDS database connection

--- include/dbcon.php
+++ include/dbcon.php
@@ -1,6 +1,6 @@
 <?php 
 // server, user, password, database, port
-$con = mysqli_connect("localhost", "root", "", "project_library", 3307);
+$con = mysqli_connect("library-db.cfik8aewcbl1.ap-south-1.rds.amazonaws.com","admin","Admin098","library-db");
 
 if (!$con) {
     die("Connection failed: " . mysqli_connect_error());
