e56aa261734a5501e5c0ace8eddbe560d5690f61
Sneha Walunj <snehawalunj01@gmail.com>
2026-02-20 19:31:01 +0000

    index

--- index.php
+++ index.php
@@ -1,4 +1,8 @@
 <?php
+ini_set('display_errors',1);
+ini_set('display_startup_errors',1);
+error_reporting(E_ALL);
+<?php
 // --- PHP LOGIN LOGIC ---
 // This block should be at the very top of the file, before any HTML.
 
